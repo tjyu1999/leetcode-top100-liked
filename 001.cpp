b4ed5b52d5b93d7b15025b511dbbc2a9f12ef279
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-03-25 19:23:32 +0800

    Add files via upload

---
+++ 001.cpp
@@ -0,0 +1,30 @@
+# include<iostream>
+# include<vector>
+# include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> record;
+        
+        for(int i=0; i!= nums.size(); i++){
+            if (record.count(target - nums[i])){
+                return {record[target - nums[i]], i};
+            }
+            record[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+int main(){
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 13;
+
+    for (int idx : Solution().twoSum(nums, target)){
+        cout << idx << endl;
+    }
+
+    return 0;
+}
