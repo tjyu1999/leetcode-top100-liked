cdad1814910a51896beb582b33eb292537ab9e80
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-04-03 13:31:18 +0800

    Add files via upload

---
+++ 225.cpp
@@ -0,0 +1,46 @@
+# include<iostream>
+# include<queue>
+using namespace std;
+
+class MyStack{
+public:
+    MyStack(){
+        
+    }
+    
+    void push(int x){
+        q.push(x);
+
+        for(int i=0; i<q.size()-1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop(){
+        int val = q.front();
+        q.pop();
+
+        return val;
+    }
+    
+    int top(){
+        return q.front();
+    }
+    
+    bool empty(){
+        return q.empty();
+    }
+
+private:
+    queue<int> q;
+};
+
+int main(){
+    MyStack stk;
+    stk.push(1);
+    stk.push(2);
+
+    cout << stk.top() << endl;
+    cout << stk.empty() << endl;
+}
