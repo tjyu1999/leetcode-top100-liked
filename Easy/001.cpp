050e4c61153463518526c096f253a5010087cb5c
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-04-06 20:31:46 +0800

    Update 001.cpp

--- Easy/001.cpp
+++ Easy/001.cpp
@@ -3,9 +3,9 @@
 # include<unordered_map>
 using namespace std;
 
-class Solution {
+class Solution{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int>& nums, int target){
         unordered_map<int, int> record;
         
         for(int i=0; i < nums.size(); i++){
@@ -22,9 +22,7 @@ int main(){
     vector<int> nums = {2, 7, 11, 15};
     int target = 13;
 
-    for (int idx:Solution().twoSum(nums, target)){
-        cout << idx << endl;
-    }
+    for(int idx:Solution().twoSum(nums, target)) cout << idx << endl;
 
     return 0;
 }
