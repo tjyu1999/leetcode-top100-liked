99874848d63ffd746c5f1f6d6bb42454c9619546
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-06-08 00:56:12 +0800

    Add files via upload

---
+++ cpp/0136.cpp
@@ -0,0 +1,18 @@
+# include<iostream>
+# include<vector>
+using namespace std;
+
+class Solution{
+public:
+    int singleNumber(vector<int>& nums){
+        int res = 0;
+        for(auto n: nums) res ^= n;
+        
+        return res;
+    }
+};
+
+int main(){
+    vector<int> nums = {4, 1, 2, 1, 2};
+    cout << Solution().singleNumber(nums) << endl;
+}
