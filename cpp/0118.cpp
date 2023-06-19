9817bac464cd32506ac73cc2d2fcd3d3b1c7d6b2
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-06-20 00:34:17 +0800

    Add files via upload

--- cpp/0118.cpp
+++ cpp/0118.cpp
@@ -8,14 +8,8 @@ public:
         vector<vector<int>> res(numRows);
         
         for(int i=0; i<numRows; i++){
-            res[i].resize(i + 1);
-            
-            if(i == 0) res[i][0] = 1;
-            if(i == 1) res[i][0] = res[i][1] = 1;
-            else{
-                res[i][0] = res[i][i] = 1;
-                for(int j=1; j<i; j++) res[i][j] = res[i-1][j-1] + res[i-1][j];
-            }
+            res[i].resize(i + 1, 1);
+            for(int j=1; j<i; j++) res[i][j] = res[i-1][j-1] + res[i-1][j];
         }
 
         return res;
