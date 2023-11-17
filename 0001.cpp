c71d04a2c13c330275d437abb1cb89b70fa5ecb0
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-11-17 11:33:52 +0100

    Add files via upload

---
+++ 0001.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution{
+public:
+    vector<int> twoSum(vector<int>& nums, int target){
+        unordered_map<int, int> map;
+    
+        for(int i = 0; i < nums.size(); i++){
+            if(map.count(target - nums[i])) return {map[target - nums[i]], i};
+            map[nums[i]] = i;
+        }
+        
+        return {};
+    }
+};
+
+int main(){
+    vector<int> nums;
+    int val;
+    int target;
+    
+    while(cin >> val){
+        nums.push_back(val);
+        if(cin.get() == '\n') break;
+    }
+    cin >> target;
+    for(auto i: Solution().twoSum(nums, target)) cout << i << endl;
+}
