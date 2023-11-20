d84b7ca2617af308926f2c505e9d6c8861b75efb
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-11-20 15:44:15 +0100

    Add files via upload

---
+++ src/0283.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    void moveZeroes(vector<int>& nums){
+        int j = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != 0){
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+                j++;
+            }
+        }
+    }
+};
+
+int main(){
+    int val;
+    vector<int> nums;
+    
+    while(cin >> val){
+        nums.push_back(val);
+        if(cin.get() == '\n') break;
+    }
+    Solution().moveZeroes(nums);
+    for(auto n: nums) cout << n << endl;
+}
