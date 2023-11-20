1e7af79951b86514d59dfcf01eda69d72ed58560
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-11-20 15:38:42 +0100

    Update 0169.cpp

--- src/0169.cpp
+++ src/0169.cpp
@@ -8,10 +8,11 @@ public:
     int majorityElement(vector<int>& nums){
         unordered_map<int, int> cnt;
         
-        for(int n : nums) {
+        for(int n : nums){
             cnt[n]++;
             if(cnt[n] > nums.size() / 2) return n;
         }
+        
         return 0;
     }
 };
