e4e8226ba778481e9ba8decc571e90638fe93b7c
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2023-11-20 13:35:57 +0100

    Add files via upload

---
+++ src/0704.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+public:
+    int search(vector<int>& nums, int target){
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid;
+        
+        while(left <= right){
+            mid = left + (right - left) / 2;
+
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target) left = mid + 1;
+            else if(nums[mid] > target) right = mid - 1;
+        }
+        
+        return -1;
+    }
+};
+
+int main(){
+    vector<int> nums;
+    int val;
+    int target;
+    
+    while(cin >> val){
+        nums.push_back(val);
+        if(cin.get() == '\n') break;
+    }
+    cin >> target;
+    cout << Solution().search(nums, target) << endl;
+}
