01cfe4a92ad26e1225269bf61243508cbb9b510b
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-02-20 11:06:00 +0800

    Add files via upload

---
+++ src/0078.cpp
@@ -0,0 +1,19 @@
+class Solution{
+public:  
+    vector<vector<int>> subsets(vector<int>& nums){
+        vector<vector<int>> res;
+        dfs(nums, 0, {}, res);
+        
+        return res;
+    }
+
+private:
+    void dfs(const vector<int>& nums, int depth, vector<int>&& path, vector<vector<int>>& res){
+        res.push_back(path);
+        for(int i = depth; i < nums.size(); ++i){
+            path.push_back(nums[i]);
+            dfs(nums, i + 1, move(path), res);
+            path.pop_back();
+        }
+    }
+};
