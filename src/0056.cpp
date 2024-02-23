f6f7d92a67e6d45c0bf3a7d29fdeddeb857a894a
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-02-23 23:59:03 +0800

    Add files via upload

---
+++ src/0056.cpp
@@ -0,0 +1,14 @@
+class Solution{
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals){
+        vector<vector<int>> res;
+        ranges::sort(intervals);
+        
+        for(vector<int>& interval : intervals){
+            if(res.empty() || res.back()[1] < interval[0]) res.push_back(interval);
+            else res.back()[1] = max(res.back()[1], interval[1]);
+        }
+        
+        return res;
+    }
+};
