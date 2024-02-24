533845aa142d055c6b8ba71b92cf9fe248118f37
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-02-24 21:30:37 +0800

    Add files via upload

--- src/0136.cpp
+++ src/0136.cpp
@@ -1,7 +1,3 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-
 class Solution{
 public:
     int singleNumber(vector<int>& nums){
@@ -11,14 +7,3 @@ public:
         return res;
     }
 };
-
-int main(){
-    int val;
-    vector<int> nums;
-    
-    while(cin >> val){
-        nums.push_back(val);
-        if(cin.get() == '\n') break;
-    }
-    cout << Solution().singleNumber(nums) << endl;
-}
