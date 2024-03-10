6a8578104a1a36aaeb61901994ca351d2cd89876
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-10 23:29:29 +0800

    Update 0739.cpp

--- src/0739.cpp
+++ src/0739.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures){
+    vector<int> dailyTemperatures(vector<int> &temperatures){
         vector<int> res(temperatures.size());
         stack<int> st;
         
