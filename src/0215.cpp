8242b1f893ddb87983e6de4172709e83b25a1846
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-10 23:54:00 +0800

    Update 0215.cpp

--- src/0215.cpp
+++ src/0215.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:
-    int findKthLargest(vector<int>& nums, int k){
+    int findKthLargest(vector<int> &nums, int k){
         priority_queue<int, vector<int>, greater<int>> pq;
 	for(int n : nums){
 	    pq.push(n);
