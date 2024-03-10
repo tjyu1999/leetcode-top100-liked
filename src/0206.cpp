ef059bf81be73c7327efc714e8a56452497f8d83
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-10 23:29:47 +0800

    Update 0206.cpp

--- src/0206.cpp
+++ src/0206.cpp
@@ -1,10 +1,10 @@
 class Solution{
 public:
-    ListNode* reverseList(ListNode* head){
-        ListNode* prev = nullptr;
+    ListNode* reverseList(ListNode *head){
+        ListNode *prev = nullptr;
         
         while(head){
-            ListNode* next = head->next;
+            ListNode *next = head->next;
             head->next = prev;
             prev = head;
             head = next;
