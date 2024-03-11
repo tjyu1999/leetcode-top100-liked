5d0f4a03dc052a271996eb76f7757bbac78cafb5
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-11 17:54:36 +0800

    Update 0104.cpp

--- src/0104.cpp
+++ src/0104.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:
-    int maxDepth(TreeNode* root){
+    int maxDepth(TreeNode *root){
         if(!root) return 0;
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
