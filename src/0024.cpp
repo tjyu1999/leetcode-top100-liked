9e3a606f8329f13169e272c8c166554378096b1e
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-12 00:06:01 +0800

    Update 0024.cpp

--- src/0024.cpp
+++ src/0024.cpp
@@ -1,9 +1,9 @@
 class Solution{
 public:
-    ListNode* swapPairs(ListNode* head){
+    ListNode* swapPairs(ListNode *head){
         if(!head || !head->next) return head;
         
-        ListNode* curr = head->next;
+        ListNode *curr = head->next;
         head->next = swapPairs(curr->next);
         curr->next = head;
         
