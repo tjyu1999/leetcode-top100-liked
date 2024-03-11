ddce4dbe104439e50e6f861c90bc72827ec89916
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-12 00:05:45 +0800

    Update 0021.cpp

--- src/0021.cpp
+++ src/0021.cpp
@@ -1,12 +1,6 @@
-struct ListNode{
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(nullptr) {}
-};
-
 class Solution{
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2){
+    ListNode* mergeTwoLists(ListNode *list1, ListNode *list2){
         if(!list1 && !list2) return nullptr;
         else if(!list1) return list2;
         else if(!list2) return list1;
