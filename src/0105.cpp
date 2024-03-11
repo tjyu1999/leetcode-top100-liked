2a3df479ebda02a7bc524861e0d707231b0db79c
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-11 17:51:14 +0800

    Update 0105.cpp

--- src/0105.cpp
+++ src/0105.cpp
@@ -1,16 +1,15 @@
 class Solution{
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder){
+    TreeNode* buildTree(vector<int> &preorder, vector<int> &inorder){
         unordered_map<int, int> map;
         for(int i = 0; i < inorder.size(); i++) map[inorder[i]] = i;
         
-        return builder(preorder, 0, preorder.size() - 1,
-                       inorder, 0, inorder.size() - 1, map);
+        return builder(preorder, 0, preorder.size() - 1, inorder, 0, inorder.size() - 1, map);
     }
     
 private:
-    TreeNode* builder(const vector<int>& preorder, int pre_s, int pre_e,
-                      const vector<int>& inorder, int in_s, int in_e, const unordered_map<int, int>& map){
+    TreeNode* builder(const vector<int> &preorder, int pre_s, int pre_e,
+                      const vector<int> &inorder, int in_s, int in_e, const unordered_map<int, int> &map){
         if(pre_s > pre_e) return nullptr;
         
         const int root_val = preorder[pre_s];
