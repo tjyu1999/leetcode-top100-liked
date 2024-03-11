08e61460b008482d2a61361967cc5a0f8103bb07
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-11 17:52:28 +0800

    Update 0014.cpp

--- src/0014.cpp
+++ src/0014.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:
-    string longestCommonPrefix(vector<string>& strs){
+    string longestCommonPrefix(vector<string> &strs){
         if(strs.empty()) return "";
         
         for(int i = 0; i < strs[0].length(); i++){
