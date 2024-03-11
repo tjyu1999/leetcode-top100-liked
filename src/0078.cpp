5902be59656c7c0b66f4d9715519d46059bef854
tjyu1999 <87049229+tjyu1999@users.noreply.github.com>
2024-03-11 17:54:15 +0800

    Update 0078.cpp

--- src/0078.cpp
+++ src/0078.cpp
@@ -1,6 +1,6 @@
 class Solution{
 public:  
-    vector<vector<int>> subsets(vector<int>& nums){
+    vector<vector<int>> subsets(vector<int> &nums){
         vector<vector<int>> res;
         dfs(nums, 0, {}, res);
         
@@ -8,7 +8,7 @@ public:
     }
 
 private:
-    void dfs(const vector<int>& nums, int depth, vector<int>&& path, vector<vector<int>>& res){
+    void dfs(const vector<int> &nums, int depth, vector<int> &&path, vector<vector<int>> &res){
         res.push_back(path);
         for(int i = depth; i < nums.size(); ++i){
             path.push_back(nums[i]);
